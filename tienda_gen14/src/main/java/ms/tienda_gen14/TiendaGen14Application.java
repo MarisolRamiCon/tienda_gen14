9f5dba7a646f1b6ec7c87742f2e13a69ee563874
MarisolRamiCon <mari12011991@hotmail.com>
2025-02-25 19:29:07 -0600

    Subiendo la estructura del proyecto

---
+++ tienda_gen14/src/main/java/ms/tienda_gen14/TiendaGen14Application.java
@@ -0,0 +1,13 @@
+package ms.tienda_gen14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaGen14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaGen14Application.class, args);
+	}
+
+}
---
+++ tienda_gen14/src/test/java/ms/tienda_gen14/TiendaGen14ApplicationTests.java
@@ -0,0 +1,13 @@
+package ms.tienda_gen14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaGen14ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
